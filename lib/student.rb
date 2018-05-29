4216a7020574e18930f1679c34196616a948ae64
Spencer Goodman <spencerwgoodman@icloud.com>
2018-05-29 03:38:54 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -53,9 +53,9 @@ class Student
     DB[:conn].execute(sql, self.name, self.grade, self.id)
   end
 
-  def self.create(name:, grade:)
-  student = Student.new(name, grade)
-  student.save
-  student
-end
+  def self.create(name, grade)
+    student = Student.new(name, grade)
+    student.save
+    student
+  end
 end
