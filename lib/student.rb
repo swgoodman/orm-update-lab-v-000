f3e2604ad4a664edb839ff4f57faf39c87ba135a
Spencer Goodman <spencerwgoodman@icloud.com>
2018-05-29 03:30:15 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -38,7 +38,7 @@ class Student
     SQL
 
     DB[:conn].execute(sql)
-    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
+      @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
   end
 
 end
