55a4567c82bdfc017063500c6ca236a8e48a3934
Spencer Goodman <spencerwgoodman@icloud.com>
2018-05-29 03:54:00 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -66,8 +66,17 @@ class Student
     Student.new(name, grade, id)
   end
 
-  def self.find_by_name
+  def self.find_by_name(name)
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE name = ?
+      LIMIT 1
+    SQL
 
+    DB[:conn].execute(sql, name).map do |row|
+      self.new_from_db(row)
+    end.first
   end
 
 end
