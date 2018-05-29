249508f062412f5d9e7205883fc5d61c62eedc61
Spencer Goodman <spencerwgoodman@icloud.com>
2018-05-29 03:51:57 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -61,9 +61,9 @@ class Student
 
   def self.new_from_db(row)
     id = row[0]
-    name = row[1]
+    name =  row[1]
     grade = row[2]
-    Student.new(name, grade, id)
+    Student.new(id, name, grade)
   end
 
 end
